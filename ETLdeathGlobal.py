7563c6af5ecc9a91873ae6094041fded048b893c
KadunaPoly <43528902+KadunaPoly@users.noreply.github.com>
2023-04-03 23:08:18 +0200

    Create ETLdeathGlobal.py

---
+++ ETLdeathGlobal.py
@@ -0,0 +1,5 @@
+import pandas as pd
+df = pd.read_csv('C:\\Users\\FITEC\\OneDrive\\Documents\\Projet ElasticSearch\\time_series_covid19_death_global_with_etl.csv')
+df_melted = pd.melt(df, id_vars=['Province/State','Country/Region', 'Lat', 'Long'], value_vars= row[4:])
+#print(df_melted)
+df_melted.to_csv('C:\\Users\\FITEC\\OneDrive\\Documents\\Projet ElasticSearch\\time_series_covid19_death_global_with_etl.csv', index=False)
